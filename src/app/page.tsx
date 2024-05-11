73c0233555f4b14a66fde86fc8f3296118aa6213
Bri <brianjcarroll0@gmail.com>
2024-05-11 18:35:30 -0400

    init landing site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,97 @@
-import Image from "next/image";
+import { Button } from "../components/ui/button";
+import { Check } from "lucide-react";
+import { Inter } from "next/font/google";
+import Link from "next/link";
 
-export default function Home() {
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+import {
+  AppleLogo,
+  Bitcoin,
+  Computer,
+  Eagle,
+  Keys,
+  Logo,
+  Penguin,
+  Play,
+  Shield,
+} from "../components/icons";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const features = [
+  {
+    title: "Secure single sign on",
+    description:
+      "Keep your nostr keys safe in one place when you sign in to desktops apps",
+  },
+  {
+    title: "Multi-account management",
+    description: "create and manage multiple nostr keys with ease",
+  },
+  {
+    title: "Seamless payments",
+    description: "Zaps and lightning support with nostr wallet connect",
+  },
+];
+
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div
+      className={`min-w-screen relative z-10 flex h-full min-h-screen w-full flex-col justify-between bg-white text-[#29333D] ${inter.className}`}
+    >
+      <Header />
+      <div className="m-auto my-8 items-center justify-center p-4 text-center">
+        <div className="m-auto flex max-w-screen-md flex-col gap-y-4">
+          <div className="flex flex-row items-center justify-center gap-x-2">
+            <Logo /> <h1 className="text-2xl">Keystache</h1>
+          </div>
+
+          <h2 className="text-4xl font-semibold md:text-5xl">
+            Your sovereign desktop companion for nostr applications
+          </h2>
+
+          <div className="mt-4 flex flex-col items-center justify-center gap-y-2">
+            <div className="flex flex-col gap-x-2 gap-y-2 md:flex-row">
+              <a href="/Keystache_0.0.0_aarch64.dmg" download>
+                <Button className="m-auto self-start bg-[#5247E6] p-6">
+                  <AppleLogo />
+                  <span className="ml-2">Download for Mac</span>
+                </Button>
+              </a>
+              <Link href="/demo">
+                <Button className="m-auto self-start bg-[#FD865F] p-6 hover:bg-[#fd6c3d]">
+                  <Play />
+                  <span className="ml-2">Watch Demo</span>
+                </Button>
+              </Link>
+            </div>
+            <div className="text-xs text-[#364C63] text-muted">
+              Coming soon to Windows
+            </div>
+          </div>
+        </div>
+        <div className="m-auto mt-10 flex max-w-md flex-col gap-y-6">
+          {features.map((feature, i) => (
+            <div className="flex flex-row text-left" key={i}>
+              <Check className="mr-2 shrink-0 text-[#5247E6]" />
+              <div className="flex-auto">
+                <span className="font-bold">{feature.title}</span>{" "}
+                <span className="text-[#364C63]">{feature.description}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <Footer />
+      <div className="absolute bottom-0 -z-10 hidden h-screen w-full md:block">
+        <Eagle className="absolute bottom-40" />
+        <Bitcoin className="absolute bottom-0" />
+        <Shield className="absolute bottom-0" />
+        <Penguin className="absolute bottom-36 right-0" />
+        <Computer className="absolute bottom-0 right-0" />
+        <Keys className="absolute bottom-0 right-0" />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
